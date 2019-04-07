68059c9f38622ff37f4c5675d64c8a4a29e6df3d
James Gung <gungjm@gmail.com>
2019-04-07 19:33:56 -0400

    Ignore continuation/reference rels

--- src/main/java/io/github/clearwsd/semlink/aligner/RelAligner.java
+++ src/main/java/io/github/clearwsd/semlink/aligner/RelAligner.java
@@ -3,6 +3,7 @@ package io.github.clearwsd.semlink.aligner;
 import com.google.common.base.Preconditions;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import io.github.clearwsd.propbank.type.ArgNumber;
 import io.github.clearwsd.semlink.PropBankPhrase;
@@ -19,7 +20,9 @@ public class RelAligner implements PbVnAligner {
 
     @Override
     public void align(@NonNull PbVnAlignment alignment) {
-        List<PropBankPhrase> rels = alignment.byNumber(ArgNumber.V);
+        List<PropBankPhrase> rels = alignment.byNumber(ArgNumber.V).stream()
+                .filter(rel -> !rel.isContinuation())
+                .filter(rel -> !rel.isReference()).collect(Collectors.toList());
         List<FramePhrase> verbs = alignment.bySyntacticType(VerbNetSyntaxType.VERB);
         Preconditions.checkState(rels.size() == 1 && verbs.size() == 1);
         alignment.add(rels.get(0), verbs.get(0));
