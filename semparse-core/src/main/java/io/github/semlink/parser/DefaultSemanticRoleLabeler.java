d810f5c3aa4649dc70142985500bb33fd9970e9f
James Gung <gungjm@gmail.com>
2019-05-02 17:45:21 -0400

    Update copyright

--- semparse-core/src/main/java/io/github/semlink/parser/DefaultSemanticRoleLabeler.java
+++ semparse-core/src/main/java/io/github/semlink/parser/DefaultSemanticRoleLabeler.java
@@ -1,6 +1,29 @@
+/*
+ * Copyright 2019 James Gung
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.semlink.parser;
 
 import com.google.common.base.Preconditions;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import io.github.clearwsd.SensePrediction;
 import io.github.clearwsd.type.DefaultNlpFocus;
 import io.github.clearwsd.type.DepNode;
@@ -10,11 +33,6 @@ import io.github.semlink.app.Chunking;
 import io.github.semlink.app.ShallowParser;
 import io.github.semlink.app.ShallowParserUtils;
 import io.github.semlink.type.ITokenSequence;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 
