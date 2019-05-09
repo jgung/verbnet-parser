74c02727a2aabc9f2643c669cd254b711a65fc82
James Gung <gungjm@gmail.com>
2019-05-09 10:25:33 -0400

    Map lemmas of heavy nouns to corresponding verbs

--- semparse-core/src/main/java/io/github/semlink/util/TsvUtils.java
+++ semparse-core/src/main/java/io/github/semlink/util/TsvUtils.java
@@ -20,11 +20,14 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /**
  * TSV utilities.
@@ -36,17 +39,24 @@ public final class TsvUtils {
 
     public static Map<String, Map<String, String>> tsv2Map(String path, int key1Col, int key2Col, int valCol) throws IOException {
         Map<String, Map<String, String>> result = new HashMap<>();
-        for (String line : Files.readAllLines(Paths.get(path), Charset.defaultCharset())) {
-            line = line.trim();
-            if (line.isEmpty()) {
-                continue;
-            }
-            String[] fields = line.split("\t");
+        for (String[] fields : readTsv(path)) {
             if (fields.length < 3) {
                 continue;
             }
             Map<String, String> subMap = result.computeIfAbsent(fields[key1Col], ignored -> new HashMap<>());
             subMap.put(fields[key2Col], fields[valCol]);
+        }
+        return result;
+    }
+
+    public static List<String[]> readTsv(@NonNull String path) throws IOException {
+        List<String[]> result = new ArrayList<>();
+        for (String line : Files.readAllLines(Paths.get(path), Charset.defaultCharset())) {
+            line = line.trim();
+            if (line.isEmpty()) {
+                continue;
+            }
+            result.add(line.split("\t"));
         }
         return result;
     }
